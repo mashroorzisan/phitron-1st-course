8cee50e44f156ad714fb17421a0ef887589a6751
zisan <mashroorzisan@gmail.com>
2023-11-06 02:37:42 +0600

    last commit

--- Module-20/Magical-tree.c
+++ Module-20/Magical-tree.c
@@ -1,16 +1,33 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+
+
+int main() {
     int n;
+    int count=0;
     scanf("%d", &n);
-    int s=5,k;
-    for(int i=1;i<s;i++){
-        for(){
+    for(int i=1;i<=n;i++){
+        if(i % 2 !=0){
+            count++;
+        }
+    }
+    int lines = count + 5;  
+    for (int i = 1; i <= lines; i++) {
+        for (int j = 1; j <= lines - i; j++) {
+            printf(" ");
+        }
+        for (int j = 1; j <= 2*i - 1; j++) {
             printf("*");
         }
         printf("\n");
-        s--;
     }
-
-
-    return 0 ;
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <=5; j++) {
+            printf(" ");
+        }
+        for(int k = 1;k<=n;k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
 }
