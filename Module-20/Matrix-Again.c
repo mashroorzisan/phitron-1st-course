fb48afc3442b49732d0a261985e849f88a94de3f
zisan <mashroorzisan@gmail.com>
2023-11-06 00:53:20 +0600

    fourth problem

---
+++ Module-20/Matrix-Again.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+    int r, c;
+    scanf("%d %d", &r, &c);
+    int ar[100][100];
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &ar[i][j]);
+        }
+    }
+
+    for(int i=0;i<c;i++){
+        printf("%d ", ar[r-1][i]);
+    }
+    printf("\n");
+    for(int i = 0;i<r;i++){
+        printf("%d ",ar[i][c-1]);
+    }
+    return 0;
+}
