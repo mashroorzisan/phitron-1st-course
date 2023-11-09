3772cab9349c284b953d357941bd317e7889a505
zisan <mashroorzisan@gmail.com>
2023-11-09 12:41:18 +0600

    conceptual

---
+++ conceptual sessions/week-1-c-1/chatgpt-r1-evenodd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d", &a);
+    
+    if(a%2 == 0){
+        printf("Even");
+        return 0;
+    }
+    printf("Odd");
+    return 0;
+}
---
+++ conceptual sessions/week-1-c-1/test.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    
+    int a, b, c;
+    printf("Enter a number: ");
+    scanf("%d/%d", &a, &b);
+    
+    printf(" a : %d b: %d", a,b);
+
+    return 0;
+}
